eaafc0b7c35e6daa0bc9213cad523a9db2096683
dzhivanyan <dzhivanyan>
2025-02-07 16:27:10 +0300

    add badge, divider

---
+++ src/shared/ui/Badge/badge.stories.ts
@@ -0,0 +1,20 @@
+import { Badge } from './badge';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'Shared/Badge',
+  component: Badge,
+  parameters: {
+    layout: 'padded',
+  },
+} satisfies Meta<typeof Badge>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Base: Story = {
+  args: {
+    text: 'Badge',
+  },
+};
